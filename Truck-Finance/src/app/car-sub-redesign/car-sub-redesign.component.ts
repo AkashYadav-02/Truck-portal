6586dddaaf90bbd6bcf5048f60d434e23d35de74
Akash Yadav <Akash.Yadav@asl.mum.sst>
2023-10-23 09:28:30 +0530

    changes  done

--- Truck-Finance/src/app/car-sub-redesign/car-sub-redesign.component.ts
+++ Truck-Finance/src/app/car-sub-redesign/car-sub-redesign.component.ts
@@ -356,6 +356,10 @@ export class CarSubRedesignComponent implements OnInit {
 
   ngOnInit() {
     const labels = document.querySelectorAll('label');
+    
+    this.currentCarDetails=this.carDetails.filter((car)=> {return car.name === this.techDetailsParameter.name});
+    console.log(this.currentCarDetails);
+    this.variantName=this.currentCarDetails[0].select_variant[0];
 
     labels.forEach((label) => {
       this.renderer.listen(label, 'click', () => {
@@ -388,9 +392,6 @@ export class CarSubRedesignComponent implements OnInit {
     });
     
 
-    this.currentCarDetails=this.carDetails.filter((car)=> {return car.name === this.techDetailsParameter.name});
-    console.log(this.currentCarDetails);
-    this.variantName=this.currentCarDetails[0].select_variant[0];
     
     // this.variantName = this.currentCarDetails[0].select_variant[this.variant];
     
