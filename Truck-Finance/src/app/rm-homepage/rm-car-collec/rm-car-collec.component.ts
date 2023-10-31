d6e92ac42abad12f0b5200e3825ff342fde49c90
DipikaAdak2311 <dipikaadak23@gmail.com>
2023-10-31 16:31:39 +0530

    changes

--- Truck-Finance/src/app/rm-homepage/rm-car-collec/rm-car-collec.component.ts
+++ Truck-Finance/src/app/rm-homepage/rm-car-collec/rm-car-collec.component.ts
@@ -20,7 +20,7 @@ export class RmCarCollecComponent {
     {
       name:"Trailer",
 
-      price: "366,267",
+      price: "231,595",
 
       imgUrl: "../../assets/Truck5.png",
 
@@ -40,7 +40,7 @@ export class RmCarCollecComponent {
 
       name:"T680",
 
-      price: "172,784",
+      price: "190,233",
 
       imgUrl: "../../assets/Truck2.png",
 
